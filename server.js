c2a8d68c5996a8d33ddf1299388fe551999ffbc1
laurensallart <laurens.allart@gmail.com>
2017-12-18 13:09:48 +0100

    test

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
+// test
 app.listen(process.env.PORT || 8080);
